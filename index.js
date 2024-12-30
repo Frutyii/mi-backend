50090cd433b27002d19c5d48ed71c24c21f31851
Frutyii <tomatesjuanlu@gmail.com>
2024-12-30 15:45:50 +0100

    Eliminados node_modules del seguimiento de git

--- index.js
+++ index.js
@@ -98,8 +98,6 @@ const authenticate = (req, res, next) => {
 
 // Conexión a MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
     serverSelectionTimeoutMS: 5000,
     socketTimeoutMS: 45000,
 })
